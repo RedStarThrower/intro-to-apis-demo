571143a245e5dbccc4f2bfec79eb0ca49b743f91
Natalia Podosinnikova <Natalia@Natalias-MacBook-Air.local>
2016-02-23 15:32:55 -0600

    initial upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+console.log($)
+
+var apikey = "325ade0da4514bb29ff036144a8bc016"
+
+var url = "http://openstates.org/api/v1/legislators/?state=tx&apikey=325ade0da4514bb29ff036144a8bc016"
+
+var congressionalPromise = $.getJSON(url)
+
+var legislatorToHTML = function(legislatorObj) {
+	var newString = '<div class = "legislatorContainer"><img src="' + legislatorObj.photo_url + '"><p class="legislatorName">' + legislatorObj.full_name + '</p></div>'
+	return newString
+}
+
+var handleData = function(resultArray) {
+	var htmlString = ""
+	for (var i = 0; i < resultArray.length; i++) {
+		var legislatorObj = resultArray[i]		
+		htmlString += legislatorToHTML(legislatorObj)
+	}
+	var containerEl = document.querySelector("#container")
+	containerEl.innerHTML = htmlString	
+}
+
+congressionalPromise.then(handleData) 
